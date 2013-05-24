951aaf1e8e93fa510197d98e9485b2077d456ed7
Peter Sevens <peter@sevensict.nl>
2013-05-24 22:25:43 +0200

    php53: use /etc/php53 for PHP confs of this version
    Keeps it isolated, reduces the risk of mixing together configuration files
    of different PHP versions (or using the configuration file for the wrong
    version altogether) which would probably result in mayhem.

--- php53/doinst.sh
+++ php53/doinst.sh
@@ -3,6 +3,6 @@
 # elif [ "`cat etc/httpd/mod_php.conf 2> /dev/null`" = "" ]; then
 #   cp -a etc/httpd/mod_php.conf.example etc/httpd/mod_php.conf
 # fi
-if [ ! -r etc/httpd/php.ini ]; then
-   cp -a etc/httpd/php.ini-production etc/httpd/php.ini
+if [ ! -r etc/php53/php.ini ]; then
+   cp -a etc/php53/php.ini-production etc/php53/php.ini
 fi
